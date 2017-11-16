9d315c07699d05352437e0bfe42aa17279c94e5f
lojavier <loj90@sbcglobal.net>
2017-11-16 15:37:46 -0800

    Update gauge stuff

--- assign10/main.js
+++ assign10/main.js
@@ -20,7 +20,8 @@ function initMap()
     });
 }
 
-$(document).ready(function() {
+function displayHighchart()
+{
     $.get("reservoir_conditions.json", function(JSONObj) {
         var last_update = JSONObj.last_update;
         var categories = [];
@@ -86,20 +87,88 @@ $(document).ready(function() {
 
             }]
         });
+    });
+}
 
-        $(".container").append("<div id=\"test\" class=\"row\"></div>");
+var gaugeObj = [];
+function setGaugeObjects(obj)
+{
+    gaugeObj = obj;
+}
+
+function displayLiquidGauge()
+{
+    $.get("reservoir_conditions.json", function(JSONObj) {
+        var config = liquidFillGaugeDefaultSettings();
+        config.circleThickness = 0.15;
+        config.waveCount = 2;
+        config.waveAnimateTime = 1000;
+        config.waveHeight = 0.05;
+        config.waveAnimate = true;
+        config.waveRise = false;
+        config.waveHeightScaling = false;
+        config.waveOffset = 0.25;
+        config.waveTextColor = "#FFFFFF";
+
+        var rowNum = 0;
+        var colNum = 0;
+        var maxCol = 4;
+        var maxRows = Math.ceil(JSONObj.reservoir_conditions.length / maxCol);
+        var gaugeObj = [];
+
+        $(".container-fluid").append("<div class=\"row\" id=\"row-"+rowNum+"\"></div>");
 
         for (var i = 0; i < JSONObj.reservoir_conditions.length; i++)
         {
-            var max_capacity = JSONObj.reservoir_conditions[i].max_capacity;
-            var current_capacity = max_capacity * (JSONObj.reservoir_conditions[i].percent_capacity / 100);
-            var historical_capacity = current_capacity * (100 / JSONObj.reservoir_conditions[i].percent_historical_avg);
-            categories.push(JSONObj.reservoir_conditions[i].name);
-            max_capacities.push(max_capacity);
-            current_capacities.push(current_capacity);
-            historical_capacities.push(historical_capacity);
+            name = JSONObj.reservoir_conditions[i].name;
+            percent_capacity = JSONObj.reservoir_conditions[i].percent_capacity;
+
+            $("#row-"+rowNum).append("<div class=\"col-sm-3\" id=\"gauge-col-"+i+"\"></div>")
+            $("#gauge-col-"+i).append("<h4>"+name+"</h4>");
+            $("#gauge-col-"+i).append("<svg id=\"fill-gauge-"+i+"\" width=\"100%\"></svg>");
+            var gauge = loadLiquidFillGauge("fill-gauge-"+i, percent_capacity, config);
+            gaugeObj.push(gauge);
+
+            if(colNum < (maxCol-1))
+            {
+                colNum++;
+            }
+            else
+            {
+                colNum = 0;
+                rowNum++;
+                $(".container-fluid").append("<div class=\"row\" id=\"row-"+rowNum+"\"></div>");
+            }
         }
 
-        $("#test").append("<h1>TEST</h1>");
+        setGaugeObjects(gaugeObj);
     });
+}
+
+function updateLiquidGauge()
+{
+    $.get("reservoir_conditions.json", function(JSONObj) {
+        var config = liquidFillGaugeDefaultSettings();
+        config.circleThickness = 0.15;
+        config.waveCount = 2;
+        config.waveAnimateTime = 1000;
+        config.waveHeight = 0.05;
+        config.waveAnimate = true;
+        config.waveRise = false;
+        config.waveHeightScaling = false;
+        config.waveOffset = 0.25;
+        config.waveTextColor = "#FFFFFF";
+
+        for (var i = 0; i < JSONObj.reservoir_conditions.length; i++)
+        {
+            percent_capacity = JSONObj.reservoir_conditions[i].percent_capacity;
+            gaugeObj[i].update(percent_capacity);
+        }
+    });
+}
+
+$(document).ready(function() {
+    displayHighchart();
+    displayLiquidGauge();
+    setInterval(function(){displayHighchart();updateLiquidGauge();}, 10000);
 });
