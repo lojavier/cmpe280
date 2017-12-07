2283a5e82ca2d61793b1a929586c1a222a413895
lojavier <loj90@sbcglobal.net>
2017-12-07 01:09:32 -0800

    Update 12/07/2017

--- project/js/main.js
+++ project/js/main.js
@@ -220,9 +220,8 @@ function getLyftEstimates() {
 	var xhttp = new XMLHttpRequest();
 	xhttp.onreadystatechange = function() {
 		if (this.readyState == 4 && this.status == 200) {
-			// warningMessage.innerHTML = this.responseText;
 			var JSONObj = JSON.parse(this.responseText);
-	        displayLyftEstimates(JSONObj);
+	        displayLyftEstimates(JSONObj,lyft_client_id);
 		}
 	};
 	xhttp.open("GET", lyft_estimates_url, true);
@@ -230,7 +229,7 @@ function getLyftEstimates() {
 	xhttp.send();
 }
 
-function displayLyftEstimates(JSONObj) {
+function displayLyftEstimates(JSONObj,lyft_client_id) {
 
 	var rideIndex = {
 		lyft_line:0,
@@ -268,9 +267,16 @@ function displayLyftEstimates(JSONObj) {
 		}
 		results[index] = JSONObj.cost_estimates[i];
 	}
+
+	var lyft_deep_link = "https://lyft.com/ride";
+	// if( screen.width <= 480 ) {     
+	// 	lyft_deep_link += "ul/";
+	// }
 	
 	for (var i = 0; i < results.length; i++) 
 	{
+		var lyft_app_link = lyft_deep_link+"?id="+results[i].ride_type+"&partner="+lyft_client_id+"&pickup[latitude]="+pickup_lat+"&pickup[longitude]="+pickup_lng+"&destination[latitude]="+dropoff_lat+"&destination[longitude]="+dropoff_lng;
+
 		$("#lyft-estimate-"+i).html("");
 		$("#lyft-estimate-"+i).append(" \
 			<div class=\"w3-container w3-white w3-center\"> \
@@ -281,7 +287,7 @@ function displayLyftEstimates(JSONObj) {
             	<p class=\"w3-text-green\">$"+(results[i].estimated_cost_cents_min/100).toFixed(2)+" (Estimated Ride Cost)</p> \
                 <p>"+Math.ceil((results[i].estimated_duration_seconds/60))+" mins (Estimated Ride Duration)</p> \
                 <p>"+results[i].estimated_distance_miles+" miles (Estimated Ride Distance)</p> \
-            	<button class=\"w3-button w3-margin-bottom w3-blue-grey\">Request Ride <span class=\"glyphicon glyphicon-new-window\"></span></button> \
+            	<button class=\"w3-button w3-margin-bottom w3-blue-grey\" onclick=\"window.open(\'"+lyft_app_link+"\');\">Request Ride <span class=\"glyphicon glyphicon-new-window\"></span></button> \
             </div>");
 	}
 }
@@ -294,6 +300,70 @@ function getUberEstimates() {
 	     'https://api.uber.com/v1.2/estimates/price?start_latitude=37.3120321&start_longitude=-121.95222860000001&end_latitude=37.3844772&end_longitude=-121.93272890000003&server_token=RUOqYOd-IgBcjFQ4J8mHc7ixW3vD9nRX3-f_Llrn'
 	*/
 
+	/*
+	curl -H "Authorization: Bearer KA.eyJ2ZXJzaW9uIjoyLCJpZCI6InB6YnJrZzNlU0RLSlpacGZkNnhacUE9PSIsImV4cGlyZXNfYXQiOjE1MTUyMjMyNDUsInBpcGVsaW5lX2tleV9pZCI6Ik1RPT0iLCJwaXBlbGluZV9pZCI6MX0.IO22hUn1WE8v4CioDz-S4O_CfWl6hoQSJFxm4BteMvw" \
+	"https://api.uber.com/v1.2/products?latitude=37.7752278&longitude=-122.4197513"
+
+	curl "https://api.uber.com/v1.2/products?latitude=37.7752278&longitude=-122.4197513&access_token=KA.eyJ2ZXJzaW9uIjoyLCJpZCI6InB6YnJrZzNlU0RLSlpacGZkNnhacUE9PSIsImV4cGlyZXNfYXQiOjE1MTUyMjMyNDUsInBpcGVsaW5lX2tleV9pZCI6Ik1RPT0iLCJwaXBlbGluZV9pZCI6MX0.IO22hUn1WE8v4CioDz-S4O_CfWl6hoQSJFxm4BteMvw"
+	*/
+
+	/*
+	curl -X POST \
+     -H "Authorization: Bearer KA.eyJ2ZXJzaW9uIjoyLCJpZCI6InB6YnJrZzNlU0RLSlpacGZkNnhacUE9PSIsImV4cGlyZXNfYXQiOjE1MTUyMjMyNDUsInBpcGVsaW5lX2tleV9pZCI6Ik1RPT0iLCJwaXBlbGluZV9pZCI6MX0.IO22hUn1WE8v4CioDz-S4O_CfWl6hoQSJFxm4BteMvw" \
+     -H 'Accept-Language: en_US' \
+     -H 'Content-Type: application/json' \
+     -d '{
+       "start_latitude": 37.7752278,
+       "start_longitude": -122.4197513,
+       "end_latitude": 37.7773228,
+       "end_longitude": -122.4272052
+     }' "https://api.uber.com/v1.2/requests/estimate"
+
+	curl -X PUT 'https://sandbox-api.uber.com/v1.2/requests/estimate' \
+     -H 'Authorization: Bearer KA.eyJ2ZXJzaW9uIjoyLCJpZCI6InB6YnJrZzNlU0RLSlpacGZkNnhacUE9PSIsImV4cGlyZXNfYXQiOjE1MTUyMjMyNDUsInBpcGVsaW5lX2tleV9pZCI6Ik1RPT0iLCJwaXBlbGluZV9pZCI6MX0.IO22hUn1WE8v4CioDz-S4O_CfWl6hoQSJFxm4BteMvw' \
+     -H 'Accept-Language: en_US' \
+     -H 'Content-Type: application/json' \
+	-d '{
+	  "fare": {
+	    "value": 5.73,
+	    "fare_id": "d30e732b8bba22c9cdc10513ee86380087cb4a6f89e37ad21ba2a39f3a1ba960",
+	    "expires_at": 1476953293,
+	    "display": "$5.73",
+	    "currency_code": "USD",
+	    "breakdown": [
+	     {
+	       "type": "promotion",
+	       "value": -2.00,
+	       "name": "Promotion"
+	     },
+	     {
+	       "type": "base_fare",
+	       "notice": "Fares are slightly higher due to increased demand",
+	       "value": 7.73,
+	       "name": "Base Fare"
+	     }
+	   ]
+	  },
+	  "trip": {
+	    "distance_unit": "mile",
+	    "duration_estimate": 540,
+	    "distance_estimate": 2.39
+	  },
+	  "pickup_estimate": 2
+	}'
+
+	curl -X POST 'https://sandbox-api.uber.com/v1.2/requests' \
+     -H 'Content-Type: application/json' \
+     -H 'Authorization: Bearer KA.eyJ2ZXJzaW9uIjoyLCJpZCI6InB6YnJrZzNlU0RLSlpacGZkNnhacUE9PSIsImV4cGlyZXNfYXQiOjE1MTUyMjMyNDUsInBpcGVsaW5lX2tleV9pZCI6Ik1RPT0iLCJwaXBlbGluZV9pZCI6MX0.IO22hUn1WE8v4CioDz-S4O_CfWl6hoQSJFxm4BteMvw' \
+     -d '{ "fare_id": "abcd", "product_id": "a1111c8c-c720-46c3-8534-2fcdd730040d", "start_latitude": 37.761492, "start_longitude": -122.423941, "end_latitude": 37.775393, "end_longitude": -122.417546 }'
+
+    */
+
+    /*
+	https://login.uber.com/oauth/v2/authorize?client_id=3JajizBicUQTjxJrh8R0iDgt_HUBCJWS&response_type=code&redirect_uri=https://52.90.66.201
+    */
+
+	var uber_access_token = "KA.eyJ2ZXJzaW9uIjoyLCJpZCI6InB6YnJrZzNlU0RLSlpacGZkNnhacUE9PSIsImV4cGlyZXNfYXQiOjE1MTUyMjMyNDUsInBpcGVsaW5lX2tleV9pZCI6Ik1RPT0iLCJwaXBlbGluZV9pZCI6MX0.IO22hUn1WE8v4CioDz-S4O_CfWl6hoQSJFxm4BteMvw";
 	var uber_client_id = "3JajizBicUQTjxJrh8R0iDgt_HUBCJWS";
 	var uber_client_secret = "5NAqxlNLlRMXne7TVyUzQgxR91fN6tMzkKQFgXNM";
 	var uber_server_token = "RUOqYOd-IgBcjFQ4J8mHc7ixW3vD9nRX3-f_Llrn";
@@ -318,6 +388,52 @@ function getUberEstimates() {
 	    	console.log(JSON.stringify(error));
 	    }
 	});
+
+	// $.ajax({
+	// 	url: "https://api.uber.com/v1.2/requests/estimate",
+	// 	type: "POST",
+	// 	headers: {
+	// 		'Authorization': 'Bearer ' + uber_access_token
+	// 	},
+	// 	contentType: "application/json",
+	// 	data: {
+	//         start_latitude: pickup_lat,
+	// 	    start_longitude: pickup_lng,
+	// 	    end_latitude: dropoff_lat,
+	// 	    end_longitude: dropoff_lng
+	//     },
+	// 	success: function(result,status,xhr) {
+	// 		console.log(JSON.stringify(result));
+	//         // displayUberEstimates(result,uber_client_id);
+	//     },
+	//     error: function(xhr,status,error) {
+	//     	console.log(JSON.stringify(xhr));
+	//     	console.log(JSON.stringify(status));
+	//     	console.log(JSON.stringify(error));
+	//     }
+	// });
+
+	// $.ajax({
+	// 	url: "https://api.uber.com/v1.2/requests/estimate",
+	// 	type: "GET",
+	// 	data: {
+	//         start_latitude: pickup_lat,
+	// 	    start_longitude: pickup_lng,
+	// 	    end_latitude: dropoff_lat,
+	// 	    end_longitude: dropoff_lng,
+ //    		token_type: "Bearer",
+ //    		access_token: uber_access_token
+	//     },
+	// 	success: function(result,status,xhr) {
+	// 		console.log(JSON.stringify(result));
+	//         // displayUberEstimates(result,uber_client_id);
+	//     },
+	//     error: function(xhr,status,error) {
+	//     	console.log(JSON.stringify(xhr));
+	//     	console.log(JSON.stringify(status));
+	//     	console.log(JSON.stringify(error));
+	//     }
+	// });
 }
 
 function displayUberEstimates(JSONObj,uber_client_id) {
@@ -367,10 +483,13 @@ function displayUberEstimates(JSONObj,uber_client_id) {
 		results[index] = JSONObj.prices[i];
 	}
 
+	var uber_deep_link = "https://m.uber.com/";
+	if( screen.width <= 480 ) {     
+		uber_deep_link += "ul/";
+	}
 	for (var i = 0; i < results.length; i++) 
 	{
-		var uri = "https://m.uber.com/ul/?client_id="+uber_client_id+"&action=setPickup&pickup[latitude]="+pickup_lat+"&pickup[longitude]="+pickup_lng+"&pickup[formatted_address]="+$("#pickup-input").val()+"&dropoff[latitude]="+dropoff_lat+"&dropoff[longitude]="+dropoff_lng+"&dropoff[formatted_address]="+$("#dropoff-input").val()+"&product_id="+results[i].product_id;
-		// var uri = "https://m.uber.com/?client_id="+uber_client_id+"&action=setPickup&pickup[latitude]="+pickup_lat+"&pickup[longitude]="+pickup_lng+"&pickup[formatted_address]="+$("#pickup-input").val()+"&dropoff[latitude]="+dropoff_lat+"&dropoff[longitude]="+dropoff_lng+"&dropoff[formatted_address]="+$("#dropoff-input").val()+"&product_id="+results[i].product_id;
+		var uri = uber_deep_link+"?client_id="+uber_client_id+"&action=setPickup&pickup[latitude]="+pickup_lat+"&pickup[longitude]="+pickup_lng+"&pickup[formatted_address]="+$("#pickup-input").val()+"&dropoff[latitude]="+dropoff_lat+"&dropoff[longitude]="+dropoff_lng+"&dropoff[formatted_address]="+$("#dropoff-input").val()+"&product_id="+results[i].product_id;
 		var uber_app_link = encodeURI(uri);
 
 		$("#uber-estimate-"+i).html("");
@@ -383,8 +502,7 @@ function displayUberEstimates(JSONObj,uber_client_id) {
             	<p class=\"w3-text-green\">$"+results[i].low_estimate+" - "+results[i].high_estimate+" (Estimated Ride Cost)</p> \
                 <p>"+Math.ceil(results[i].duration/60)+" mins (Estimated Ride Duration)</p> \
                 <p>"+results[i].distance+" miles (Estimated Ride Distance)</p> \
-            	<button class=\"w3-button w3-margin-bottom w3-blue-grey\">Request Ride <span class=\"glyphicon glyphicon-new-window\"></span></button> \
-            	<a href=\""+uber_app_link+"\" target=\"_blank\">TEST</a> \
+            	<button class=\"w3-button w3-margin-bottom w3-blue-grey\" onclick=\"window.open(\'"+uber_app_link+"\');\">Request Ride <span class=\"glyphicon glyphicon-new-window\"></span></button> \
             </div>");
 	}
 	// https://m.uber.com/ul/?client_id=<CLIENT_ID>&action=setPickup&pickup[latitude]=37.775818&pickup[longitude]=-122.418028&pickup[nickname]=UberHQ&pickup[formatted_address]=1455%20Market%20St%2C%20San%20Francisco%2C%20CA%2094103&dropoff[latitude]=37.802374&dropoff[longitude]=-122.405818&dropoff[nickname]=Coit%20Tower&dropoff[formatted_address]=1%20Telegraph%20Hill%20Blvd%2C%20San%20Francisco%2C%20CA%2094133&product_id=a1111c8c-c720-46c3-8534-2fcdd730040d&link_text=View%20team%20roster&partner_deeplink=partner%3A%2F%2Fteam%2F9383
